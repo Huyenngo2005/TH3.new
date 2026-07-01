3e28bd77ddf795b8acacb517a73e5acb3596b2de
Huyenngo2005 <ngothihuyenym@gmail.com>
2026-07-01 13:00:44 +0700

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AppBWeb.Default" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>AppB - Server</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>AppB - Server</h2>
+            <asp:Label ID="lblInfo" runat="server" Text="Chưa có dữ liệu từ Client" Font-Bold="true" /><br /><br />             
+            <asp:Label ID="lblReceived" runat="server" /><br /><br />
+            Phản hồi: <asp:TextBox ID="txtFeedback" runat="server" Width="300px" /><br /><br />
+            <asp:Button ID="btnSendBack" runat="server" Text="Gửi phản hồi về AppA" OnClick="btnSendBack_Click" /><br /><br />                        
+            <asp:Label ID="lblStatus" runat="server" ForeColor="Green" /> 
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.UI;
+
+namespace AppBWeb
+{
+    public partial class Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string name = Request.QueryString["name"];
+                string price = Request.QueryString["price"];
+                if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(price))
+                {
+                    lblInfo.Text = "Dữ liệu nhận được từ AppA:";
+                    lblReceived.Text = $"Sản phẩm: <b>{name}</b> - Giá: <b>{price}</b>";
+                }
+            }
+        }
+
+        protected void btnSendBack_Click(object sender, EventArgs e)
+        {
+            string feedback = txtFeedback.Text.Trim();
+
+            // Gói phản hồi lại và đẩy trình duyệt quay trở về AppA
+            string url = $"http://localhost:81/AppAWeb/Default.aspx?feedback={Uri.EscapeDataString(feedback)}";
+            Response.Redirect(url);
+        }
+    }
+}
